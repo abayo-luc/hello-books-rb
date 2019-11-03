330d8ca39f9053d5c10a9453a4b539d81b08d3ab
abayo-luc <luc.bayo@gmail.com>
2019-11-03 17:00:57 +0200

    Token on account confirmation

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,13 +2,12 @@ class Api::V1::SessionsController < ApplicationController
   skip_before_action :authenticate_request, only: %i[create]
   def create
     user = User.find_by email: params[:email]
+    raise ExceptionHandler::NotConfirmed unless user&.confirmed?
     if user&.valid_password?(params[:password])
       jwt = WebToken.encode(user)
       render :create, status: :ok, locals: { token: jwt }
     else
       raise 'Invalid email or password'
     end
-  rescue StandardError => e
-    raise ExceptionHandler::CustomError, e.message
   end
 end
