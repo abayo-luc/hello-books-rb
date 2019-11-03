bc116667bbe886cd73731ae3c4e9e7b38d8566f2
abayo-luc <luc.bayo@gmail.com>
2019-11-03 23:09:10 +0200

    Forbid unconfirmed account to login

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,12 +2,12 @@ class Api::V1::SessionsController < ApplicationController
   skip_before_action :authenticate_request, only: %i[create]
   def create
     user = User.find_by email: params[:email]
-    raise ExceptionHandler::NotConfirmed unless user&.confirmed?
     if user&.valid_password?(params[:password])
+      raise ExceptionHandler::NotConfirmed unless user&.confirmed?
       jwt = WebToken.encode(user)
       render :create, status: :ok, locals: { token: jwt }
     else
-      raise 'Invalid email or password'
+      raise ExceptionHandler::CustomError, 'Invalid email or password'
     end
   end
 end
